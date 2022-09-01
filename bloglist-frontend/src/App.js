03a7d442e2798dc165a10a557ed607e3d597e714
Lam-Laptop <lam.tran@aalto.fi>
2022-09-01 18:37:10 +0300

    fixed create

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -89,7 +89,7 @@ const App = () => {
                 <h2>blogs</h2> 
                 <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
                 <Toggleable buttonLabel="new blog">
-                    <BlogForm handleCreate={handleCreate} />
+                    <BlogForm handleCreate={handleCreate} user={user}/>
                 </Toggleable>
                 
                 {
